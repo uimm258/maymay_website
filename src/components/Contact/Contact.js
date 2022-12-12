ae5866101a5b6ed4fa2570c294aaaf19b586a2bc
MayMay Liu <maymayliu@MayMays-MacBook-Air.local>
2022-12-11 23:31:04 -0800

    add resume

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,16 +1,20 @@
 import { contact } from '../../portfolio'
 import './Contact.css'
+import pdfFile from './Maymay_Liu.pdf'
 
 const Contact = () => {
   if (!contact.email) return null
 
   return (
     <section className='section contact center' id='contact'>
-      <h2 className='section__title'>Contact</h2>
+      <h2 className='section__title'>Contact & Resume</h2>
       <a href={`mailto:${contact.email}`}>
         <span type='button' className='btn btn--outline'>
           Email me
         </span>
+      </a>
+      <a href = {pdfFile} target = "_blank" rel="noreferrer">
+        <span type='button' className='btn btn--outline'> My Resume</span>
       </a>
     </section>
   )
